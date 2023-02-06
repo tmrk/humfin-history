3c89647b07d018292e4a569ad88e5ef2975d76f7
Tamas Marki <tamas.marki@gmail.com>
2023-02-06 12:27:11 +0545

    Fixed CPI adjusment, better tooltips

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Humanitarian Finance History
+
+This page displays a line chart showing how much funds (in Swiss Franc) have been used for humanitarian response since 1919, adjusted for inflation.
+
+Although only one data source is used (i.e. the appeals data exposed by the [IFRCGo API](https://goadmin.ifrc.org/docs/#api-v2-appeal-list)), it is still indicative of historical trends given the Red Cross Movement's long-standing presence in humanitarian reponse, and this is the only currently available dataset dating all the way back to the aftermath of World War I. 
+
+(For modern datasets that include much more aspects of aid funding, see the 
+[International Aid Transparency Initiative](https://github.com/IATI).)
+
+To adjust for inflation, the CPI data published by the [Federal Statistical Office of Switzerland](https://www.bfs.admin.ch/asset/de/cc-d-05.02.08) is used.
+
+## Demo
+
+https://tmrk.github.io/humfin-history/
+
+## Planned
+
+* Add some basic UI (e.g. switch inflation adjustment on/off)
+* Add currency conversion
+* Improve design to make the page more shareable
--- src/app.js
+++ src/app.js
@@ -2,36 +2,45 @@ import 'chartist/dist/index.css';
 import 'chartist-plugin-tooltips-updated/dist/chartist-plugin-tooltip.css';
 import { LineChart } from 'chartist';
 import ChartistTooltip from 'chartist-plugin-tooltips-updated';
+import { el } from 'redom';
 
 // Load inflation data for adjusting historical figures
 //import CPIdata from './modules/FetchCPI.js';
 import CPIdata from './assets/CPIdata.json';
-console.log(CPIdata)
 
 import AppealData from './assets/AppealData_20230205.json';
+
+const inflationAdjusted = true;
 
 const startDates = [];
 const amounts = [];
 const chartData = {};
-const inflationAdjusted = false;
+const lastYearInCPIdata = Math.max.apply(null, CPIdata.map(year => year.year));
 
-function adjustForInflation (amount, fromYear, toYear) {
-  fromYear = Number(fromYear);
-  const lastYearInCPIdata = Math.max.apply(null, CPIdata.map(year => year.year));
-  toYear = Number(toYear || lastYearInCPIdata);
+function format (number, decimals) {
+  if (number) {
+    let parts = parseFloat(number).toFixed(decimals).toString().split('.');
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return parts.join('.');
+  } else return '0';
+}
 
-  if (fromYear >= toYear) return amount;
+function adjustForInflation (amount, baseYear, targetYear) {
+  baseYear = Number(baseYear);
+  targetYear = Number(targetYear || lastYearInCPIdata);
+
+  if (baseYear >= targetYear) return amount;
   else {
-    const fromYearData = CPIdata.find(year => year.year === fromYear);
-    const latestBase = Object.keys(fromYearData).reduce((a, b) => 
-      new Date(fromYearData[a]) > new Date(fromYearData[b]) ? a : b
-    );
-    return amount / 100 * fromYearData[latestBase];
+    const targetYearData = CPIdata.find(year => year.year === targetYear);
+    const baseYearData = CPIdata.find(year => year.year === baseYear);
+    const latestBaseIndex = Object.keys(baseYearData)
+      .filter(key => key !== 'year')
+      .reduce((a, b) => new Date(a) > new Date(b) ? a : b);
+    return amount / baseYearData[latestBaseIndex] * targetYearData[latestBaseIndex];
   }
 }
 
 function processAppeals (data) {
-  console.log(data);
   const filtered = data.sort((a, b) => 
     new Date(a.start_date).getTime() - new Date(b.start_date).getTime()
   );
@@ -48,28 +57,49 @@ function processAppeals (data) {
     else chartData[year] = amount;
   }
 
-  console.log(filtered)
-  console.log(Object.values(chartData));
-  console.log(chartData);
-
   new LineChart(
-    '#root',
+    '#chart',
     {
       labels: Object.keys(chartData),
       series: [
-        Object.values(chartData).map((value, index) => ({
-          meta: Object.keys(chartData)[index],
-          value: inflationAdjusted ? adjustForInflation(value, Object.keys(chartData)[index]) : value
-        }))
+        Object.values(chartData).map((value, index) => {
+
+          const year = Object.keys(chartData)[index];
+          const amount = inflationAdjusted ? adjustForInflation(value, year) : value;
+          const tooltip = el('div', [
+            el('div.year', year),
+            el('div.meta', [
+              el('div.amount', format(amount) + ' CHF'),
+              el('div.info', 
+                inflationAdjusted ? 'Adjusted for inflation (' + year + '–' + lastYearInCPIdata + ')' 
+                : 'Original amount (' + year + ')'
+              )
+            ])
+          ]);
+                    
+          return ({
+            meta: tooltip.innerHTML,
+            value: amount
+          })
+        })
       ]
     },
     {
-      axisY: { ticks: [10000, 20000, 100000] },
+      axisX: {
+        labelInterpolationFnc: function(value) {
+          if (value.toString().endsWith('0')) return value;
+        }
+      },      axisY: {
+        labelInterpolationFnc: function(value) {
+          return format(value / 1000000) + ' M';
+        }
+      },
       low: 0,
       showArea: true,
       plugins: [
         ChartistTooltip({
-          currency: 'CHF '
+          anchorToPoint: false,
+          metaIsHTML: true
         })
       ]
     }
